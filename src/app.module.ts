fa8ea59696dcc06c8034dba15f55277daf6b36cd
deborahpviana <debora_a_pereira@hotmail.com>
2021-06-13 21:39:16 -0300

    adicionando autenticaçao

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,18 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 import { MongooseModule } from '@nestjs/mongoose';
+import { ProductsModule } from './products/products.module';
+import { AuthModule } from './auth/auth.module';
 
 import config from './config/config';
 
 @Module({
-  imports: [MongooseModule.forRoot(config.mongoUrl), UsersModule],
+  imports: [
+    MongooseModule.forRoot(config.mongoUrl),
+    UsersModule,
+    ProductsModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,36 +6,44 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { FindIdParams } from './dto/find-id-user.dto';
 
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOne(@Param() params: FindIdParams) {
     return this.usersService.findOne(params.id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(@Param() params: FindIdParams, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(params.id, updateUserDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param() params: FindIdParams) {
     return this.usersService.remove(params.id);
